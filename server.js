d468d29becb281cfac3b5335db8d961f8384b159
Officier Azarov <elisabethfainstein@gmail.com>
2020-04-25 19:37:17 +0200

    prepare for deploy

--- server.js
+++ server.js
@@ -29,7 +29,11 @@ app.get('/api/memberAdd', (req, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-    
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'built', 'index.html'));
+    })
 }
 
 app.listen(PORT, console.log('Server started'));
