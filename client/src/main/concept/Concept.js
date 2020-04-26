199bf2052353a76192789ec680fd5deefbab942c
Officier Azarov <elisabethfainstein@gmail.com>
2020-04-27 00:47:14 +0200

    add concept animation on hover

--- client/src/main/concept/Concept.js
+++ client/src/main/concept/Concept.js
@@ -16,11 +16,52 @@ import gameboy from '../../resources/img/icons/gameboy.png';
 
 class Concept extends React.Component {
 
+    state = {
+        isNightSchoolOn: false,
+        isSocialOn: false,
+        isLearningOn: false,
+        isCoolContentOn: false
+    }
+
     componentDidMount() {
         // passes the location fo the app (current path) to the parent (App.js)
         emitCurrentPath(this.props);
 
         window.scrollTo({top: 0, behavior: 'smooth'});
+    }
+
+    hoverEffectOn = (e) => {
+        console.log(e.target.getAttribute('id'));
+        switch (e.target.getAttribute('id')) {
+            case 'safe-space':
+                this.setState({isSocialOn: true});
+                break;
+            case 'muscle':
+                this.setState({isNightSchoolOn: true, isLearningOn: true});
+                break;  
+            case 'culture':
+                this.setState({isCoolContentOn: true});
+                break;    
+            case 'level-up':
+                this.setState(        
+                    {
+                        isNightSchoolOn: true,
+                        isSocialOn: true,
+                        isLearningOn: true,
+                        isCoolContentOn: true
+                    });    
+                break;
+        }
+
+    }
+
+    hoverEffectOff = (e) => {
+        this.setState({
+            isNightSchoolOn: false,
+            isSocialOn: false,
+            isLearningOn: false,
+            isCoolContentOn: false
+        });
     }
 
     render() {
@@ -32,19 +73,27 @@ class Concept extends React.Component {
                 <div id="how-items-container">
                     <div id="principles">
                         <ul>
-                            <li>
+                            <li id="safe-space" 
+                                onMouseEnter={this.hoverEffectOn}
+                                onMouseLeave={this.hoverEffectOff}>
                                 <img src={community} alt="" />
                                 <p>Fabriquer un safe-space d'entraide et de bienveillance entre devs juniors</p>
                             </li>
-                            <li>
+                            <li id="muscle"
+                                onMouseEnter={this.hoverEffectOn}
+                                onMouseLeave={this.hoverEffectOff}>
                                 <img src={weight} alt="" />
                                 <p>Aider les devs juniors à muscler leurs savoirs en software et&nbsp;hardware</p>
                             </li>
-                            <li>
+                            <li id="culture"
+                                onMouseEnter={this.hoverEffectOn}
+                                onMouseLeave={this.hoverEffectOff}>
                                 <img src={geek} alt="" />
                                 <p>Analyser et décrypter les codes de la culture geek</p>
                             </li>
-                            <li>
+                            <li id="level-up"
+                                onMouseEnter={this.hoverEffectOn}
+                                onMouseLeave={this.hoverEffectOff}>
                                 <img src={stairs} alt="" />
                                 <p>Motiver les devs juniors à continuer à apprendre en parallèle de leur
                         job</p>
@@ -53,19 +102,19 @@ class Concept extends React.Component {
                     </div>
                     <div id="implementations">
                         <ul>
-                            <li>
+                            <li className={this.state.isNightSchoolOn? '':'off'}>
                                 <img src={tea} alt="" />
                                 <p>Cours du soir et workshops, en ligne et en vrai.</p>
                             </li>
-                            <li>
+                            <li className={this.state.isSocialOn? '':'off'}>
                                 <img src={chat} alt="" />
                                 <p>Wall social/Discord/autre moyen de communiquer entre juniors de façon anonyme (you name it.)</p>
                             </li>
-                            <li>
+                            <li className={this.state.isLearningOn? '':'off'}>
                                 <img src={learning} alt="" />
                                 <p>Micro-learning, exercices interactifs, sandbox - pour apprendre partout sans ZzzZ.</p>
                             </li>
-                            <li>
+                            <li className={this.state.isCoolContentOn? '':'off'}>
                                 <img src={gameboy} alt="" />
                                 <p>Des contenus cool dans ta boîte mail, ton Insta ou ton Facebook (mais il paraît que c'est pour les vieux).</p>
                             </li>
