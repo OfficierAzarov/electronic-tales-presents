b68aa08988254f245ef67cf40efcdd1bf8dda2bd
Officier Azarov <elisabethfainstein@gmail.com>
2020-04-27 20:44:26 +0200

    add github icon

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -7,9 +7,10 @@ const Footer = () => {
             <a href="#">contact@electronictales.com</a>
             <p>Made with &#10084; by Electronic Tales Team</p>
             <div>
+                <a href="https://github.com/OfficierAzarov/electronic-tales-presents"><i class="fab fa-github"></i></a>
                 <a href="https://www.linkedin.com/company/34911130"><i className="fab fa-linkedin"></i></a>
-                <a href="#"><i className="fab fa-facebook"></i></a>
-                <a href="#"><i className="fab fa-instagram"></i></a>
+                {/* <a href="#"><i className="fab fa-facebook"></i></a>
+                <a href="#"><i className="fab fa-instagram"></i></a> */}
             </div>
         </footer >
     );
