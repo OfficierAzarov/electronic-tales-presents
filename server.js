b9482f31ae657af78cf5f34c5adb6f66dc89d3fa
Officier Azarov <elisabethfainstein@gmail.com>
2020-04-30 18:34:12 +0200

    fix typo in res.sendFile

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('/*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'built', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
     })
 }
 
