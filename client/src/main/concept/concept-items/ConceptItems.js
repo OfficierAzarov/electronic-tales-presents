ff683d6231702c3320aae5c060a1d5f9cf1522dc
Officier Azarov <elisabethfainstein@gmail.com>
2020-05-11 21:30:51 +0200

    start responsive version of concepts

--- client/src/main/concept/concept-items/ConceptItems.js
+++ client/src/main/concept/concept-items/ConceptItems.js
@@ -105,45 +105,62 @@ class ConceptItems extends React.Component {
   };
 
   render() {
-    return (
-      <div
-        id="how-items-container"
-        style={{
-          opacity: this.state.isShown ? 1 : 0,
-          transform: this.state.isTranslated ? "translateY(-8%)" : "",
-        }}
-      >
-        <div id="principles">
-          <ul>
-            {this.state.principles.map((item) => (
-              <li
-                id={item.id}
-                key={item.id}
-                onMouseEnter={() => this.hoverEffectOn(item)}
-                onMouseLeave={() => this.hoverEffectOff()}
-              >
-                <img src={item.img} />
-                <p>{item.text}</p>
-              </li>
-            ))}
-          </ul>
+    if (this.props.mobile === "y") {
+      return (
+        <div id="how-items-container">
+          <div id="principles">
+            <ul>
+              {this.state.principles.map((item) => (
+                <li
+                  id={item.id}
+                  key={item.id}
+                  onMouseEnter={() => this.hoverEffectOn(item)}
+                  onMouseLeave={() => this.hoverEffectOff()}
+                >
+                  <img src={item.img} />
+                  <p>{item.text}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div id="implementations">
+            <ul>
+              {this.state.implementations.map((item) => (
+                <li id={item.id} key={item.id}>
+                  <img src={item.img} />
+                  <p>{item.text}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <div id="implementations">
-          <ul>
-            {this.state.implementations.map((item) => (
-              <li
-                id={item.id}
-                key={item.id}
-                className={this.shouldIShow(item) ? "" : "off"}
-              >
-                <img src={item.img} />
-                <p>{item.text}</p>
-              </li>
-            ))}
-          </ul>
+      );
+    } else {
+      return (
+        <div id="how-items-container">
+          <div id="principles">
+            <ul>
+              {this.state.principles.map((item) => (
+                <li id={item.id} key={item.id}>
+                  <img src={item.img} />
+                  <p>{item.text}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div id="implementations">
+            <ul>
+              {this.state.implementations.map((item) => (
+                <li id={item.id} key={item.id}>
+                  <img src={item.img} />
+                  <p>{item.text}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
