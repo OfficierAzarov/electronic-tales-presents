5431ea19acaaa8aebdb642eee426b52e264cda74
Claire-Eltale <heudeclaire@ymail.com>
2020-05-11 13:01:43 +0300

    team page

--- client/src/main/team/Team.js
+++ client/src/main/team/Team.js
@@ -4,7 +4,11 @@ import Button from "../../elements/buttons/Button";
 
 import "./Team.css";
 
-import handpoint from "../../resources/img/team/handpoint3.png";
+import handpoint from "../../resources/img/team/handpoint.png";
+import monday from "../../resources/img/team/monday.png";
+import crampon from "../../resources/img/team/crampon.png";
+import partenko from "../../resources/img/team/partenko.png";
+import azarov from "../../resources/img/team/azarov.png";
 
 class Team extends React.Component {
   render() {
@@ -20,32 +24,32 @@ class Team extends React.Component {
                 <p>Mais oui, parfaitement.</p>
               </div>
             </div>
-            <div className="member-circle deg72"></div>
+            <div className="member-circle deg72"><img className="team-pic" src={monday} alt="Monday"/></div>
             <div className="team-member" id="member2">
               <div className="team-member-text">
-                <h4 className="memberName">Claire Heude</h4>
-                <p>Testeuse de solutions pédagogiques</p>
+                <h4 className="memberName">Monday Hazard</h4>
+                <p>Éleveuse de code since 2020</p>
               </div>
             </div>
-            <div className="member-circle deg144"></div>
+            <div className="member-circle deg144"><img className="team-pic" src={crampon} alt="Le Crampon"/></div>
             <div className="team-member" id="member3">
               <div className="team-member-text">
                 <h4 className="memberName">Le crampon</h4>
                 <p>Si la start-up nation avait un visage, ce serait le sien.</p>
               </div>
             </div>
-            <div className="member-circle deg216"></div>
+            <div className="member-circle deg216"><img className="team-pic" src={partenko} alt="Partenko"/></div>
             <div className="team-member" id="member4">
               <div className="team-member-text">
                 <h4 className="memberName">Partenko</h4>
                 <p>Notre indémontable caution hardware</p>
               </div>
             </div>
-            <div className="member-circle deg288"></div>
+            <div className="member-circle deg288"><img className="team-pic" src={azarov} alt="Officier Azarov"/></div>
             <div className="team-member" id="member5">
               <div className="team-member-text">
-                <h4 className="memberName">Elisabeth Fainstein</h4>
-                <p>Un vrai dur.</p>
+                <h4 className="memberName">Officier Azarov</h4>
+                <p>Architecte technique et spirituel d'Electronic Tales</p>
               </div>
             </div>
           </div>
