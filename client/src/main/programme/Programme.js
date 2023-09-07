aa8b894459135c004253e56721339694e1300e9f
elisfainstein <elisabeth@wonderbly.com>
2023-09-07 10:59:31 +0200

    Remove platform and newsletter from why

---
+++ client/src/main/programme/Programme.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { compose } from 'recompose';
+import { withTranslation } from 'react-i18next';
+
+import withDisplay from '../../elements/HOC/withDisplay';
+import Button from '../../elements/buttons/Button';
+
+import './Programme.css';
+
+class Programme extends React.Component {
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
+  }
+
+  componentDidMount() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
+
+  scrollDown = () => {
+    this.ref.current.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
+
+  render() {
+    const { t } = this.props;
+
+    if (this.props.mobile) {
+      return <div id="tracks"></div>;
+    }
+
+    return (
+      <div id="tracks">
+        <Button text={t('programme.button')} goto="/progress" />
+      </div>
+    );
+  }
+}
+
+export default compose(withDisplay, withTranslation())(Programme);
