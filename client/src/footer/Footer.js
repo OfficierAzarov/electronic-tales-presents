00be61648f8f49b06f004662dde14f6b88a44f97
officierazarov <elisabethfainstein@gmail.com>
2023-10-18 13:27:17 +0200

    (refactor) Change div to ul

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -38,26 +38,38 @@ const Footer = (props) => {
         </div>
 
         <p>Made with &#10084; by Electronic&nbsp;Tales Team</p>
-        <div>
-          <a href="mailto:contact@electronictales.io">
-            <i className="fas fa-envelope"></i>
-          </a>
-          <a href="https://www.instagram.com/electronictales.io/">
-            <i className="fab fa-instagram"></i>
-          </a>
-          <a href="https://twitter.com/ElectronicTales">
-            <i className="fab fa-twitter"></i>
-          </a>
-          <a href="https://www.facebook.com/Electronic-Tales-100499265012350">
-            <i className="fab fa-facebook"></i>
-          </a>
-          <a href="https://www.linkedin.com/company/34911130">
-            <i className="fab fa-linkedin"></i>
-          </a>
-          <a href="https://github.com/OfficierAzarov/electronic-tales-presents">
-            <i className="fab fa-github"></i>
-          </a>
-        </div>
+        <ul>
+          <li>
+            <a href="mailto:contact@electronictales.io">
+              <i className="fas fa-envelope"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/electronictales.io/">
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/ElectronicTales">
+              <i className="fab fa-twitter"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/Electronic-Tales-100499265012350">
+              <i className="fab fa-facebook"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/company/34911130">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/OfficierAzarov/electronic-tales-presents">
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
       </footer>
     );
   }
