93add75e1b38ced25f4eb9cefef4e677059bdd22
ValdeyronAnne <annevaldeyron@gmail.com>
2023-10-31 21:12:58 +0100

    lecture icons

--- client/src/footer/Footer.js
+++ client/src/footer/Footer.js
@@ -37,37 +37,41 @@ const Footer = (props) => {
           <Link to="/team">Team</Link>
         </div>
 
-        <p>Made with &#10084; by Electronic&nbsp;Tales Team</p>
-       
+        <p>Made with &#10084; by Electronic&nbsp;Tales Team</p>     
         <ul>
           <li>
-            <a href="mailto:contact@electronictales.io">
-              <i className="fas fa-envelope" alt="lien vers notre mail" ></i>
+            <a href="mailto:contact@electronictales.io" aria-label="Lien vers notre mail">
+              <i className="fas fa-envelope" aria-hidden="true"></i>
             </a>
           </li>
+          
           <li>
-            <a href="https://www.instagram.com/electronictales.io/" role="link">
-              <i className="fab fa-instagram" alt="suivez-nous sur instagram"></i> 
+            <a href="https://www.instagram.com/electronictales.io/" aria-label="Suivez-nous sur Instagram">
+              <i className="fab fa-instagram" aria-hidden="true"></i>
             </a>
           </li>
+
           <li>
-            <a href="https://twitter.com/ElectronicTales">
-              <i className="fab fa-twitter" alt="suivez-nous sur twitter"></i>
+            <a href="https://twitter.com/ElectronicTales" aria-label="Suivez-nous sur Twitter">
+              <i className="fab fa-twitter" aria-hidden="true"></i>
             </a>
           </li>
+        
           <li>
-            <a href="https://www.facebook.com/Electronic-Tales-100499265012350">
-              <i className="fab fa-facebook" alt="suivez-nous sur facebook"></i>
+            <a href="https://www.facebook.com/Electronic-Tales-100499265012350"aria-label="Suivez-nous sur facebook">
+              <i className="fab fa-facebook" aria-hidden="true"></i>
             </a>
           </li>
+
           <li>
-            <a href="https://www.linkedin.com/company/34911130">
-              <i className="fab fa-linkedin" alt="suivez-nous sur linkedin"></i>
+            <a href="https://www.linkedin.com/company/34911130" aria-label="Suivez-nous surlinkedin">
+              <i className="fab fa-linkedin" aria-hidden="true"></i>
             </a>
           </li>
+
           <li>
-            <a href="https://github.com/OfficierAzarov/electronic-tales-presents">
-              <i className="fab fa-github" alt="lien vers notre compte github"></i>
+            <a href="https://github.com/OfficierAzarov/electronic-tales-presents" aria-label="Suivez-nous sur notre compte github">
+              <i className="fab fa-github" aria-hidden="true"></i>
             </a>
           </li>
         </ul>
